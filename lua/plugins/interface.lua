afe1895982238b72691136dd80e10a7073615d55
h1m3k0 <undefined>
2025-02-01 03:24:38 +0800

    修改tree的常用快捷键

--- lua/plugins/interface.lua
+++ lua/plugins/interface.lua
@@ -29,10 +29,64 @@ return {
   },
   {
     'nvim-neo-tree/neo-tree.nvim',
-    config = function()
+    dpendencies = { 'nvim-lua/plenary.nvim', 'nvim-tree/nvim-web-devicons', 'MunifTanjim/nui.nvim', },
+    init = function()
       vim.keymap.set('n', '<Leader>e', '<CMD>Neotree<CR>', { noremap = true } )
     end,
-    dependencies = { 'nvim-lua/plenary.nvim', 'nvim-tree/nvim-web-devicons', 'MunifTanjim/nui.nvim', },
+    opts = {
+      -- https://github.com/nvim-neo-tree/neo-tree.nvim/blob/main/lua/neo-tree/defaults.lua
+      window = {
+        mappings = {
+          ['o'] = 'open',
+          ['oc'] = 'none', ['od'] = 'none', ['og'] = 'none', ['om'] = 'none', 
+          ['on'] = 'none', ['os'] = 'none', ['ot'] = 'none',
+        },
+      },
+      filesystem = {
+        window = {
+          mappings = {
+            ['/'] = 'none',  -- 普通搜索
+            ['//'] = 'fuzzy_finder', -- fuzzy搜索
+            ['.'] = 'root',  -- 切换根目录
+            ['I'] = 'toggle_hidden',
+          },
+          fuzzy_finder_mappings = {
+          },
+        },
+      },
+      buffers = {
+        window = {
+          mappings = {
+            ['.'] = 'root',  -- 切换根目录
+          },
+        },
+      },
+      git_status = {
+        window = {
+          mappings = {},
+        },
+      },
+      document_symbols = {
+        window = {
+          mappings = {
+            ['/'] = 'none',  -- 普通搜索
+            ['//'] = 'fuzzy_finder', -- fuzzy搜索
+          },
+        },
+      },
+      commands = {
+        root = function(state)  -- 切换根目录
+          -- 判断当前是否为root节点
+          if state.tree:get_node():get_id() == state.tree:get_nodes()[1]:get_id() then
+            -- 是root, 将根目录设为上级目录
+            state.commands.navigate_up(state)
+          else 
+            -- 不是root, 将根目录切换为当前节点
+            state.commands.set_root(state)
+          end
+        end,
+      }
+    },
   },
   { 
     'folke/which-key.nvim',

b875acfede84443320562b5f8590c40f6dceebf7
h1m3k0 <undefined>
2025-02-01 03:41:47 +0800

    neovim.vim=>neovim.lua时的错误

--- lua/neovim.lua
+++ lua/neovim.lua
@@ -1,4 +1,4 @@
 -- <C-Insert>复制 
 vim.keymap.set('v', '<C-Insert>', '"+y', { noremap = true })
 -- <S-Insert>粘贴
-vim.keymap.set({'i','c'}, '<S-Insert>', '<C-R>+') 
+vim.keymap.set({'i','c'}, '<S-Insert>', '<C-R>+', { noremap = true })

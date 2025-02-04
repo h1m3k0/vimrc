bb556454e76590444d39d188be34a564bc341056
h1m3k0 <undefined>
2025-02-04 19:00:26 +0800

    blink更合理的cmdline支持

--- lua/plugins/language.lua
+++ lua/plugins/language.lua
@@ -95,21 +95,32 @@ return {
     dependencies = 'rafamadriz/friendly-snippets',
     opts = {
       keymap = {
-        preset     = 'none',
-        ['<CR>']   = { 'accept', 'fallback' },
-        ['<Tab>']  = { 'snippet_forward', 'select_and_accept', 'fallback' },
-        ['<Up>']   = { 'select_prev', 'fallback' },
-        ['<Down>'] = { 'select_next', 'fallback' },
-        ['<C-P>']  = { 'select_prev', 'fallback' },
-        ['<C-N>']  = { 'select_next', 'fallback' },
-        ['<C-B>']  = { 'scroll_documentation_up', 'fallback' },
-        ['<C-F>']  = { 'scroll_documentation_down', 'fallback' },
+        preset      = 'none',
+        ['<CR>']    = { 'accept', 'fallback' },
+        ['<Tab>']   = { 'snippet_forward', 'select_and_accept', 'fallback' },
+        ['<S-Tab>'] = { 'snippet_backward', 'fallback' },
+        ['<Up>']    = { 'select_prev', 'fallback' },
+        ['<Down>']  = { 'select_next', 'fallback' },
+        ['<C-P>']   = { 'select_prev', 'fallback' },
+        ['<C-N>']   = { 'select_next', 'fallback' },
+        ['<C-B>']   = { 'scroll_documentation_up', 'fallback' },
+        ['<C-F>']   = { 'scroll_documentation_down', 'fallback' },
+        cmdline = {
+          preset      = 'none',
+          ['<Tab>']   = { 'select_next', 'fallback' },
+          ['<S-Tab>'] = { 'select_prev', 'fallback' },
+          ['<C-P>']   = { 'select_prev', 'fallback' },
+          ['<C-N>']   = { 'select_next', 'fallback' },
+        },
       },
       completion = {
         list = {
           selection = {
-            preselect = false,
-            auto_insert = false,
+            preselect = function(ctx) 
+              return ctx.mode ~= 'cmdline' 
+              and not require('blink.cmp').snippet_active{ direction = 1 }
+            end,
+            auto_insert = function(ctx) return ctx.mode == 'cmdline' end,
           },
         },
       },

4ab7c04c11c097ef5da2f8895bef69b1aa515683
h1m3k0 <undefined>
2025-02-08 11:50:21 +0800

    分离telescope

--- lua/plugins/lua-plugins.lua
+++ lua/plugins/lua-plugins.lua
@@ -12,17 +12,6 @@ return {
     config = true,
   },
   {
-    -- 文件/关键字 搜索
-    'nvim-telescope/telescope.nvim',
-    dependencies = { 'nvim-lua/plenary.nvim' },
-    event = 'VeryLazy',
-    config = function()
-      local builtin = require('telescope.builtin')
-      vim.keymap.set('n', '<Leader>ff', builtin.find_files, {})
-      vim.keymap.set('n', '<Leader>fg', builtin.live_grep, {})
-    end,
-  },
-  {
     'echasnovski/mini.nvim',
     event = 'VeryLazy',
   },

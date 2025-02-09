d818c126b69f0ba87e2148b07aea2cdf37ae623e
h1m3k0 <undefined>
2025-02-10 00:09:38 +0800

    neovide的动画

--- lua/neovide.lua
+++ lua/neovide.lua
@@ -29,24 +29,22 @@ vim.api.nvim_create_autocmd('CmdlineLeave', {
     callback = function() vim.g.neovide_input_ime = false end,
 })
 
--- 特效配置
-
--- 位置动画长度
-vim.g.neovide_position_animation_length = 0.15
--- 滚动动画长度
-vim.g.neovide_scroll_animation_length = 0.3
+-- 窗口位置改变的动画时间
+vim.g.neovide_position_animation_length = 0
+-- scroll的动画时间
+vim.g.neovide_scroll_animation_length = 0
 -- 滚动远线
 vim.g.neovide_scroll_animation_far_lines = 1
 -- 触摸拖动超时 
 vim.g.neovide_touch_drag_timeout = 0.1
--- 动画长度
-vim.g.neovide_cursor_animation_length = 0.02
--- 动画轨迹大小
-vim.g.neovide_cursor_trail_size = 0.5
+-- 光标移动的动画时间
+vim.g.neovide_cursor_animation_length = 0
+-- 光标移动的拖尾长度
+vim.g.neovide_cursor_trail_size = 0
 -- 插入模式下制作动画
-vim.g.neovide_cursor_animate_in_insert_mode = true
+vim.g.neovide_cursor_animate_in_insert_mode = false
 -- 切换命令行也有动画
-vim.g.neovide_cursor_animate_command_line = true
+vim.g.neovide_cursor_animate_command_line = false
 -- 光标闪烁动画
 vim.g.neovide_cursor_smooth_blink = true
 -- 光标粒子 railgun torpedo pixiedust sonicboom ripple wireframe

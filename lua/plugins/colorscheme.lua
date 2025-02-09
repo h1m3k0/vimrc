539f4587d0c36b85f1db6de7f4faa07322a29eb1
h1m3k0 <undefined>
2025-02-09 21:45:28 +0800

    colorscheme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -12,21 +12,41 @@ return {
   {
     'joshdick/onedark.vim',
     event = 'VeryLazy',
+    config = function()
+--       vim.cmd.colorscheme('onedark')
+--       vim.o.background = 'dark'
+    end,
   },
   {
     'morhetz/gruvbox',
     event = 'VeryLazy',
+    config = function()
+--       vim.cmd.colorscheme('gruvbox')
+--       vim.o.background = 'light'
+    end,
   },
   {
     'altercation/vim-colors-solarized',
     event = 'VeryLazy',
+    config = function()
+--       vim.cmd.colorscheme('solarized')
+--       vim.o.background = 'light'
+    end,
   },
   {
     'projekt0n/github-nvim-theme',
     event = 'VeryLazy',
+    config = function()
+--       vim.cmd.colorscheme('github_light')
+--       vim.o.background = 'light'
+    end,
   },
   {
-    'xiantang/darcula-dark.nvim',
+    'catppuccin/nvim', name = 'catppuccin',
     event = 'VeryLazy',
-  },
+    config = function()
+--       vim.cmd.colorscheme('catppuccin')
+--       vim.o.background = 'dark'
+    end,
+  }
 }

6023649347c1584c581e4e97b531ae49c6f6950f
h1m3k0 <undefined>
2025-04-24 16:34:07 +0800

    neovim的api更新, goto_next和goto_prev将弃用, jump自带_highest, 也就不需要diagnostic_level()了

--- lua/neovim.lua
+++ lua/neovim.lua
@@ -7,23 +7,9 @@ vim.keymap.set({'i','c'}, '<S-Insert>', '<C-R>+', { noremap = true })
 -- 重绘
 vim.opt.lazyredraw = false
 
-local diagnostic_level = function()
-  local list = vim.diagnostic.get()
-  local level = vim.diagnostic.severity.HINT
-  for i, diagnostic in pairs(list) do
-    if diagnostic.severity == vim.diagnostic.severity.ERROR then
-      return vim.diagnostic.severity.ERROR
-    elseif diagnostic.severity == vim.diagnostic.severity.WARN then
-      level = vim.diagnostic.severity.WARN
-    elseif diagnostic.severity == vim.diagnostic.severity.INFO then
-      level = min(vim.diagnostic.severity.INFO, level)
-    end
-  end
-  return level
-end
 vim.keymap.set('n', '<F1>', function()
-  vim.diagnostic.goto_next({severity = diagnostic_level()})
+  vim.diagnostic.jump({count = 1, _highest = true})
 end)
 vim.keymap.set('n', '<S-F1>', function()
-  vim.diagnostic.goto_prev({severity = diagnostic_level()})
+  vim.diagnostic.jump({count = -1, _highest = true})
 end)

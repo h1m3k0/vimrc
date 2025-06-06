567418659344804ee15779d8856a166349fa483e
h1m3k0 <undefined>
2025-06-06 16:01:25 +0800

    这个文件缩进总是乱

--- lua/plugins/vim-plugins.lua
+++ lua/plugins/vim-plugins.lua
@@ -18,18 +18,18 @@ return {
         },
         init = function()
             vim.cmd [[
-            " 切换目录时自动切换vim根目录
-            let g:NERDTreeChDirMode = 2
-            " 取消CD
-            let g:NERDTreeMapCWD = '<Nop>'
-            nnoremap <Leader>e <Esc>:<C-U>NERDTree
-            nnoremap <silent> <Leader>ef <Esc>:<C-U>NERDTreeFind<CR>
-            nnoremap <silent> <Leader>eF <Esc>:<C-U>NERDTreeFind<CR>:NERDTreeVCS<CR>
-            nnoremap <silent> <Leader>ee <Esc>:<C-U>NERDTreeMirror<CR>:NERDTreeFocus<CR>
-            nnoremap <silent> <Leader>et <Esc>:<C-U>NERDTreeMirror<CR>:NERDTreeToggle<CR>
-            nnoremap <silent> <Leader>ev <Esc>:<C-U>NERDTree <C-R>=g:vimdir<CR><CR>
-            nnoremap <silent> <Leader>eh <Esc>:<C-U>NERDTree $HOME<CR>
-            nnoremap <silent> <Leader>ep <Esc>:<C-U>NERDTree <C-R>=g:vimdir<CR>/plugged<CR>
+                " 切换目录时自动切换vim根目录
+                let g:NERDTreeChDirMode = 2
+                " 取消CD
+                let g:NERDTreeMapCWD = '<Nop>'
+                nnoremap <Leader>e <Esc>:<C-U>NERDTree
+                nnoremap <silent> <Leader>ef <Esc>:<C-U>NERDTreeFind<CR>
+                nnoremap <silent> <Leader>eF <Esc>:<C-U>NERDTreeFind<CR>:NERDTreeVCS<CR>
+                nnoremap <silent> <Leader>ee <Esc>:<C-U>NERDTreeMirror<CR>:NERDTreeFocus<CR>
+                nnoremap <silent> <Leader>et <Esc>:<C-U>NERDTreeMirror<CR>:NERDTreeToggle<CR>
+                nnoremap <silent> <Leader>ev <Esc>:<C-U>NERDTree <C-R>=g:vimdir<CR><CR>
+                nnoremap <silent> <Leader>eh <Esc>:<C-U>NERDTree $HOME<CR>
+                nnoremap <silent> <Leader>ep <Esc>:<C-U>NERDTree <C-R>=g:vimdir<CR>/plugged<CR>
             ]]
         end
     },
@@ -38,14 +38,14 @@ return {
         dependencies = 'ZSaberLv0/vim-easymotion-chs',
         init = function()
             vim.cmd [[
-            " 智能大小写
-            let g:EasyMotion_smartcase = 1
-            " 禁用默认映射
-            let g:EasyMotion_do_mapping = 0
-            map  <Leader><Leader>f  <Plug>(easymotion-bd-f)
-            map  <Leader><Leader>F  <Plug>(easymotion-bd-f)
-            map  <Leader><Leader>t  <Plug>(easymotion-bd-t)
-            map  <Leader><Leader>T  <Plug>(easymotion-bd-t)
+                " 智能大小写
+                let g:EasyMotion_smartcase = 1
+                " 禁用默认映射
+                let g:EasyMotion_do_mapping = 0
+                map  <Leader><Leader>f  <Plug>(easymotion-bd-f)
+                map  <Leader><Leader>F  <Plug>(easymotion-bd-f)
+                map  <Leader><Leader>t  <Plug>(easymotion-bd-t)
+                map  <Leader><Leader>T  <Plug>(easymotion-bd-t)
             ]]
         end,
     },
@@ -65,7 +65,7 @@ return {
         'machakann/vim-highlightedyank',
         init = function()
             vim.cmd [[
-            let g:highlightedyank_highlight_duration = 2000
+                let g:highlightedyank_highlight_duration = 2000
             ]]
         end,
     },
@@ -74,8 +74,8 @@ return {
         'junegunn/vim-easy-align',
         init = function()
             vim.cmd [[
-            nmap  ga  <Plug>(EasyAlign)
-            xmap  ga  <Plug>(EasyAlign)
+                nmap  ga  <Plug>(EasyAlign)
+                xmap  ga  <Plug>(EasyAlign)
             ]]
         end,
     },
@@ -90,8 +90,8 @@ return {
         'kana/vim-arpeggio',
         config = function()
             vim.cmd [[
-            Arpeggio inoremap jk <Esc>
-            Arpeggio nnoremap io  :
+                Arpeggio inoremap jk <Esc>
+                Arpeggio nnoremap io  :
             ]]
         end
     },
@@ -99,32 +99,32 @@ return {
         'skywind3000/asyncrun.vim',
         init = function()
             vim.cmd [[
-            if has('win32')
-                " windows系统使用GBK
-                let g:asyncrun_encs = 'gbk'
+                if has('win32')
+                    " windows系统使用GBK
+                    let g:asyncrun_encs = 'gbk'
                 endif
                 " 自动打开copen
                 let g:asyncrun_open = 6
-                ]]
-            end
-        },
-        { 'skywind3000/asynctasks.vim',      dependencies = 'skywind3000/asyncrun.vim' },
-        {
-            'haya14busa/vim-asterisk',
-            init = function()
-                vim.cmd [[
+            ]]
+        end
+    },
+    { 'skywind3000/asynctasks.vim',      dependencies = 'skywind3000/asyncrun.vim' },
+    {
+        'haya14busa/vim-asterisk',
+        init = function()
+            vim.cmd [[
                 let g:asterisk#keeppos = 1 " 保持光标位置
                 map *  <Plug>(asterisk-z*)
                 map #  <Plug>(asterisk-z#)
                 map g* <Plug>(asterisk-gz*)
                 map g# <Plug>(asterisk-gz#)
-                ]]
-            end
-        },
-        {
-            'mg979/vim-visual-multi',
-            init = function()
-                vim.cmd [[
+            ]]
+        end
+    },
+    {
+        'mg979/vim-visual-multi',
+        init = function()
+            vim.cmd [[
                 let g:VM_maps                    = {}
                 " 增强VM的撤销(原生undo会有问题)
                 let g:VM_maps['Undo']            = 'u'
@@ -139,30 +139,30 @@ return {
                 let g:VM_maps['Mouse Cursor']    = '<A-LeftMouse>'
                 let g:VM_maps['Mouse Word']      = '<A-RightMouse>'
                 let g:VM_maps['Switch Mode']     = '<Tab>'
-                ]]
-            end,
-        },
-        {
-            'simeji/winresizer',
-            init = function()
-                vim.cmd [[
+            ]]
+        end,
+    },
+    {
+        'simeji/winresizer',
+        init = function()
+            vim.cmd [[
                 let g:winresizer_enable = 1
                 let g:winresizer_start_key = '<Leader>we'
                 let g:winresizer_gui_enable = 1
                 let g:winresizer_gui_start_key = '<Leader>wa'
                 let g:winresizer_vert_resize = 1
                 let g:winresizer_horiz_resize = 1
-                ]]
-            end
-        },
-        {
-            'triglav/vim-visual-increment',
-            init = function()
-                vim.cmd [[
+            ]]
+        end
+    },
+    {
+        'triglav/vim-visual-increment',
+        init = function()
+            vim.cmd [[
                 vnoremap <Leader><C-A> <Plug>VisualIncrement
                 vnoremap <Leader><C-X> <Plug>VisualDecrement
-                ]]
-            end
-        },
-        { 'preservim/vim-markdown', dependencies = 'godlygeek/tabular', },
-    }
+            ]]
+        end
+    },
+    { 'preservim/vim-markdown', dependencies = 'godlygeek/tabular', },
+}

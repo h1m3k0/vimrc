93cc7196d0ec4fe1fb57249ece8e613c4d23a5d7
h1m3k0 <undefined>
2025-06-25 11:28:01 +0800

    nvim 去掉 noice

--- lua/plugins/others.lua
+++ lua/plugins/others.lua
@@ -20,28 +20,6 @@ return {
         end,
     },
     {
-        'folke/noice.nvim',
-        dependencies = { 'MunifTanjim/nui.nvim', 'rcarriga/nvim-notify' },
-        event = 'VeryLazy',
-        opts = {
-            cmdline = {
-                enabled = false,
-            },
-            messages = {
-                enabled = false,
-            },
-            notify = {
-                enabled = false,
-            },
-            lsp = {
-                override = {
-                    ['vim.lsp.util.convert_input_to_markdown_lines'] = true,
-                    ['vim.lsp.util.stylize_markdown'] = true,
-                },
-            },
-        },
-    },
-    {
         'akinsho/bufferline.nvim',
         dependencies = 'ryanoasis/vim-devicons',
         version = '*',

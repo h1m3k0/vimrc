fb046463fc7bbca5fe1ed85936d7aef00e4bd4c5
h1m3k0 <undefined>
2025-08-08 17:09:10 +0800

    vscode主题 back/light 处理TreesitterContext

--- lua/plugins/beauty.lua
+++ lua/plugins/beauty.lua
@@ -4,25 +4,27 @@ return {
         'Mofiqul/vscode.nvim',
         lazy = false,
         priority = 1000,
-        opts = {
-            -- Enable transparent background
-            transparent = true,
-            -- Enable italic comment
-            italic_comments = true,
-            -- Enable italic inlay type hints
-            italic_inlayhints = true,
-            -- Underline `@markup.link.*` variants
-            underline_links = true,
-            -- Disable nvim-tree background color
-            disable_nvimtree_bg = true,
-            -- Apply theme colors to terminal
-            terminal_colors = true,
-            group_overrides = {
-                TreesitterContext = { bg = '#404040' },
-            },
-        },
-        config = function(_, opts)
-            require('vscode').setup(opts)
+        config = function()
+            local colors = require('vscode.colors').get_colors()
+            require('vscode').setup({
+                -- Enable transparent background
+                transparent = true,
+                -- Enable italic comment
+                italic_comments = true,
+                -- Enable italic inlay type hints
+                italic_inlayhints = true,
+                -- Underline `@markup.link.*` variants
+                underline_links = true,
+                -- Disable nvim-tree background color
+                disable_nvimtree_bg = true,
+                -- Apply theme colors to terminal
+                terminal_colors = true,
+            })
+            local load = require('vscode').load
+            require('vscode').load = function(...)
+                load(...)
+                vim.api.nvim_set_hl(0, 'TreesitterContext', { bg = vim.o.background == 'dark' and '#303030' or '#FFFFFF'})
+            end
         end,
     },
 }
